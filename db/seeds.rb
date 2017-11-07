15f2bd5d915ae20d993f4832bf7a78edc71bafda
siscely <siscely.george@gmail.com>
2017-11-07 14:57:48 +0000

    commit changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,21 @@
 require 'random_data'
 
+# Create Topics
+ 15.times do
+   Topic.create!(
+     name:         RandomData.random_sentence,
+     description:  RandomData.random_paragraph
+   )
+ end
+ topics = Topic.all
+
  # Create Posts
  post = Post.find_or_create_by!(title: "Third Post", body: "This is the third post in our system")
  
  50.times do
  
    Post.create!(
- 
+     topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
@@ -34,5 +43,6 @@ require 'random_data'
  end
  
  puts "Seed finished"
+ puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
