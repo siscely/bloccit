b49ffb32966894c8d2f2726f3a05a200e54ba372
siscely <siscely.george@gmail.com>
2017-11-08 00:16:55 +0000

    commit changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,19 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-    @posts.each_with_index do |post, index|
-      if index % 5 == 0
-        post.title = "SPAM"
-      end
-    end
-  end
   
   def create
  
      @post = Post.new
      @post.title = params[:post][:title]
      @post.body = params[:post][:body]
+     @topic = Topic.find(params[:topic_id])
+     @post.topic = @topic
+
 
  
      if @post.save
  
        flash[:notice] = "Post was saved."
-       redirect_to @post
+       redirect_to [@topic, @post]
      else
  
        flash.now[:alert] = "There was an error saving the post. Please try again."
@@ -33,6 +28,7 @@ class PostsController < ApplicationController
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
   
@@ -47,7 +43,7 @@ class PostsController < ApplicationController
  
      if @post.save
        flash[:notice] = "Post was updated."
-       redirect_to @post
+       redirect_to [@post.topic, @post]
      else
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :edit
@@ -60,7 +56,7 @@ class PostsController < ApplicationController
  
      if @post.destroy
        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-       redirect_to posts_path
+       redirect_to @post.topic
      else
        flash.now[:alert] = "There was an error deleting the post."
        render :show
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   
-  resources :topics
+  
   resources :questions
   resources :advertisements
-  resources :posts
+  resources :topics do
+ 
+  resources :posts, except: [:index]
+  end
   
   get 'about' => 'welcome#about'
   get 'welcome/contact'
