7f00db166618950c662c89964bc867128a3ff87f
siscely <siscely.george@gmail.com>
2017-11-10 21:41:02 +0000

    commit changes

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe UsersController, type: :controller do
     
     let(:new_user_attributes) do
      {
-         name: "BlocHead",
+         name: "Blochead",
          email: "blochead@bloc.io",
          password: "blochead",
          password_confirmation: "blochead"
