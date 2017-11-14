9a74313188adefa98c214c1b7b34c7355c49002a
siscely <siscely.george@gmail.com>
2017-11-14 20:55:24 +0000

    commit changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,9 @@ class PostsController < ApplicationController
   
   def create
  
-     @post = Post.new
-     @post.title = params[:post][:title]
-     @post.body = params[:post][:body]
+     
      @topic = Topic.find(params[:topic_id])
-     @post.topic = @topic
+     @post = @topic.posts.build(post_params)
      @post.user = current_user
 
 
@@ -41,9 +39,8 @@ class PostsController < ApplicationController
   
   def update
      @post = Post.find(params[:id])
-     @post.title = params[:post][:title]
-     @post.body = params[:post][:body]
- 
+     @post.assign_attributes(post_params)
+     
      if @post.save
        flash[:notice] = "Post was updated."
        redirect_to [@post.topic, @post]
@@ -65,4 +62,10 @@ class PostsController < ApplicationController
        render :show
      end
    end
+   
+  private
+   def post_params
+     params.require(:post).permit(:title, :body)
+   end
+
 end
