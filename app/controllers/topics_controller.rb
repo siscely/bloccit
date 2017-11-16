9428803247d593195ddc49a985eb4432d5aa0c70
siscely <siscely.george@gmail.com>
2017-11-16 19:56:30 +0000

    commit changes

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,7 @@ class TopicsController < ApplicationController
  
  before_action :require_sign_in, except: [:index, :show]
  
- before_action :authorize_user, except: [:index, :show]
+ before_action :authorize_create, only: [:new, :create, :destroy]
    
     def index
      @topics = Topic.all
@@ -63,9 +63,7 @@ class TopicsController < ApplicationController
      params.require(:topic).permit(:name, :description, :public)
    end
    
-   
- 
-   def authorize_user
+   def authorize_create
      unless current_user.admin?
        flash[:alert] = "You must be an admin to do that."
        redirect_to topics_path
