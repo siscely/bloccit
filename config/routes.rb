619ec80191be60ae3c36d886738aed6b2ba4fbc2
siscely <siscely.george@gmail.com>
2017-11-17 01:17:25 +0000

    commit changes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
  
      resources :posts, except: [:index]
    end
+   
+  resources :posts, only: [] do
+ 
+  resources :comments, only: [:create, :destroy]
+  end
   
   resources :topics do
  
